8fb08197696b759646a118f74a122d89f16527d0
78081 <sug2000s@lgcns.com>
2025-11-19 11:33:16 +0900

    initial commit

--- frontend/src/components/layout/MainLayout.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Header } from './Header';
-import { Sidebar } from './Sidebar';
-import styles from './MainLayout.module.css';
-export const MainLayout = ({ children }) => {
-    return (_jsxs("div", { className: styles.layout, children: [_jsx(Header, {}), _jsxs("div", { className: styles.container, children: [_jsx(Sidebar, {}), _jsx("main", { className: styles.main, children: _jsx("div", { className: styles.content, children: children }) })] })] }));
-};
